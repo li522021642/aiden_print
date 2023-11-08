8a240b2457e2d717ba4a25ac817279eff26eeba2
xingxing.li <xingxing.li@porton.cn>
2023-11-08 09:03:52 +0800

    build

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   history: {
     type: 'hash',
   },
+  hash: true, // 配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存
+  base: '/', // 设置路由前缀，通常用于部署到非根目录。
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/', // 配置 webpack 的 publicPath。当打包的时候，webpack 会在静态文件路径前面添加 publicPath 的值
   antd: {},
   access: {},
   model: {},
